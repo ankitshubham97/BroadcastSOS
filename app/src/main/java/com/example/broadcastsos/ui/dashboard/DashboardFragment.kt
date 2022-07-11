2573c125f122e41eb3243cc998f613eb67a205ae
Ankit Shubham <ankitshubham97@gmail.com>
2022-07-11 16:22:30 +0530

    refactor: remove unused viewmodel

--- app/src/main/java/com/example/broadcastsos/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/broadcastsos/ui/dashboard/DashboardFragment.kt
@@ -11,14 +11,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.example.broadcastsos.R
 import com.example.broadcastsos.databinding.FragmentDashboardBinding
 import com.example.broadcastsos.interfaces.IAuthTwitter
-import com.example.broadcastsos.models.MyViewModel
 import com.example.broadcastsos.network.Handler
+import com.example.broadcastsos.services.RestarterBroadcastReceiver
 
 
 class DashboardFragment : Fragment(), IAuthTwitter {
@@ -31,9 +30,6 @@ class DashboardFragment : Fragment(), IAuthTwitter {
     private val networkHandler = Handler(this)
     private lateinit var sharedPref: SharedPreferences;
     private var isTwitterConnected = MutableLiveData(false)
-    private val viewModel: MyViewModel by viewModels()
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -63,7 +59,7 @@ class DashboardFragment : Fragment(), IAuthTwitter {
             binding.ivDashboardIcon.setImageResource(R.mipmap.ic_launcher_disconnected_round)
         }
 
-        viewModel.startWorker()
+        RestarterBroadcastReceiver.startWorker(requireContext())
 
         return root
     }
